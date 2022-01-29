c66b51e2ce77ac0d7c1095fdc0dd26975713c945
muhammed-ayman <mae252002@gmail.com>
2022-01-29 02:18:17 +0200

    Created the solution window for the pLevelVector part

--- UI/SolutionWindow.py
+++ UI/SolutionWindow.py
@@ -24,47 +24,45 @@ class SolutionWindow(AbstractFrame):
                             font=('Calibri', 15))
         FrameTitle.place(relx=0.5, y=upMargin/4, anchor=CENTER)
 
-        # # Setting the IO Matrix Entries
-        # for i in range(self.matrixDimension):
-        #     industryLabelUp = Label(self.frame,
-        #                         text=self.industriesLabels[i],
-        #                         font=('Arial', 10), anchor=W)
-        #     industryLabelBottom = Label(self.frame,
-        #                         text=self.industriesLabels[i],
-        #                         font=('Arial', 10))
+        if self.pLevelVector:
+            solutionDim = self.pLevelVector.get_rows_dimension()
+            solutionCellHeight = 0.7*config.graphicsInfo['HEIGHT']/solutionDim
+            canvas = Canvas(self.frame,
+                            width=0.2*config.graphicsInfo['WIDTH'],
+                            height=solutionCellHeight*solutionDim)
+            canvas.place(relx=0.5,
+                        rely=0.5,
+                        anchor=CENTER)
+            # Left Matrix Boundary
+            canvas.create_line(20,5,
+                            20,solutionCellHeight*solutionDim-solutionCellHeight/2,
+                            fill="#000")
+            canvas.create_line(20,5,25,5,fill="#000")
+            canvas.create_line(20,solutionCellHeight*solutionDim-solutionCellHeight/2,
+                            25,solutionCellHeight*solutionDim-solutionCellHeight/2,
+                            fill="#000")
+            # Right Matrix Boundary
+            canvas.create_line(0.2*config.graphicsInfo['WIDTH']-20,5,
+                            0.2*config.graphicsInfo['WIDTH']-20,solutionCellHeight*solutionDim-solutionCellHeight/2,
+                            fill="#000")
+            canvas.create_line(0.2*config.graphicsInfo['WIDTH']-20,5,
+                            0.2*config.graphicsInfo['WIDTH']-25,5,
+                            fill="#000")
+            canvas.create_line(0.2*config.graphicsInfo['WIDTH']-20,solutionCellHeight*solutionDim-solutionCellHeight/2,
+                            0.2*config.graphicsInfo['WIDTH']-25,solutionCellHeight*solutionDim-solutionCellHeight/2,
+                            fill="#000")
 
-        #     for j in range(self.matrixDimension):
-        #         io_cell = Entry(self.frame)
-        #         io_cell.insert(0,0)
-        #         io_cell.place(x=leftMargin+j*(cellWidth),
-        #                     y=upMargin+i*cellHeight, width=cellWidth/2, height=cellHeight/2)
-        #         self.IOEntries.append(io_cell)
+            for i in range(solutionDim):
+                solutionLabel = Label(self.frame,
+                            text=round(self.pLevelVector[i][0],3),
+                            font=('Calibri', 11),
+                            anchor=CENTER)
+                solutionLabel.place(relx=0.5,
+                            y = upMargin + i*solutionCellHeight,
+                            height=solutionCellHeight/2,
+                            anchor=CENTER)
 
-        #     industryLabelUp.place(x=leftMargin+i*(cellWidth),
-        #                     y=0.6*upMargin,
-        #                     width=cellWidth)
-        #     industryLabelBottom.place(x=0,
-        #                     y=upMargin+i*cellHeight,
-        #                     width=leftMargin,
-        #                     height=cellHeight/2)
-        
-        # # Setting the Submission Button
-        # submissionButton = Button(self.frame,
-        #                             text="Proceed",
-        #                             command=self.checkIOInputs)
-        # submissionButton.place(relx=0.5,
-        #                             y=upMargin+10+self.matrixDimension*cellHeight,
-        #                             width=0.4*config.graphicsInfo['WIDTH'],
-        #                             height=50,
-        #                             anchor=CENTER)
-        
-        # # Demand Vector Checkbutton
-        # self.demandCheckBox = Checkbutton(self.frame,
-        #                         text="Demand Vector",
-        #                         variable=self.demandVectorState)
-        # self.demandCheckBox.place(relx=0.5,
-        #                             y=upMargin+80+self.matrixDimension*cellHeight,
-        #                             anchor=CENTER)
+
     
     def __del__(self):
         print("Solution Window Destructor Called!")
